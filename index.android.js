a1d3982e2f1c05d8f3bd9a717fb2007f9d7a13c8
Patrick O'Meara <pat@fixd.io>
2017-03-30 17:19:30 +1100

    0.0.5
    * added React back in

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
-import { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { requireNativeComponent, View} from 'react-native';
 
 var REF_PICKER = 'numberpicker';
